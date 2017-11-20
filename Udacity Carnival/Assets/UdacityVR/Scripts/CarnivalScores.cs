167ecfa393eb860ff19730edd4b9ed4aa1dc8d6f
nickcottrell <nick.a.cottrell@gmail.com>
2017-11-20 13:34:20 -0600

    added carnival project

---
+++ Udacity Carnival/Assets/UdacityVR/Scripts/CarnivalScores.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+
+public class CarnivalScores : MonoBehaviour {
+
+	[SerializeField]
+	private int TeddyBearPointsMin = 2000;
+
+	float timeLeft = 60.0f;
+
+	[SerializeField]
+	private GameObject TeddyBear;
+
+	[SerializeField]
+	private GameObject TeddyParticleSystem;
+
+	[SerializeField]
+	private GameObject TeddyBearYay;
+
+	[SerializeField]
+	private GameObject TeddyBearSad;
+
+	[SerializeField]
+	private GameObject TeddyBearBoo;
+
+	[SerializeField]
+	private TextMeshPro plinkoScore;
+
+	[SerializeField]
+	private TextMeshPro wheelScore;
+
+	[SerializeField]
+	private TextMeshPro coinScore;
+
+	[SerializeField]
+	private TextMeshPro gameTimer;
+
+	public static CarnivalScores Instance;
+
+	private int plinkoPoints;
+	private int wheelPoints;
+	private int coinPoints;
+
+	private bool gameIsActive;
+
+
+	void Awake() {
+		if (Instance == null)
+			Instance = this;
+
+			gameIsActive = true;
+			TeddyBear.SetActive(false);
+			TeddyParticleSystem.SetActive(false);
+			TeddyBearYay.SetActive(false);
+			TeddyBearSad.SetActive(false);
+			TeddyBearBoo.SetActive(false);
+
+	}
+		
+	void OnDestroy() {
+		if (Instance = this)
+			Instance = null;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		plinkoScore.text = "Plinko: " + plinkoPoints.ToString("0000");
+		wheelScore.text = "Wheel: " + wheelPoints.ToString("0000");
+		coinScore.text = "Coins: " + coinPoints.ToString("0000");
+
+		timeLeft -= Time.deltaTime;
+
+		if (timeLeft > 0 & gameIsActive == true) {
+			gameTimer.text = timeLeft.ToString("F0");
+		} else if (timeLeft < 0 & gameIsActive == true) {
+			TeddyBearSad.SetActive(true);
+			TeddyBearBoo.SetActive(true);
+			gameTimer.text = "Game Over!";
+			gameIsActive = false;
+		} else if (gameIsActive == false) {
+			//do nothing
+		}
+
+		if (plinkoPoints + wheelPoints + coinPoints >= TeddyBearPointsMin & gameIsActive == true ) {
+			gameTimer.text = "YOU WIN!";
+			TeddyBear.SetActive(true);
+			TeddyParticleSystem.SetActive(true);
+			TeddyBearYay.SetActive(true);
+			gameIsActive = false;
+		}
+	}
+
+	public void IncrementPlinkoScore(float points) {
+		if (gameIsActive == true) {
+			plinkoPoints += (int)points;
+		}
+	}
+
+	public void IncrementWheelScore(float points) {
+		if (gameIsActive == true) {	
+			wheelPoints += (int)points;
+		}
+	}
+
+	public void IncrementCoinScore() {
+		if (gameIsActive == true) {
+			coinPoints += 1000;
+		}
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ Udacity Carnival/README.txt
@@ -0,0 +1,31 @@
+Nick Cottrell's Udacity Carnival Starter Project
+
+|\     /|(  __  \ (  ___  )(  ____ \\__   __/\__   __/|\     /|
+| )   ( || (  \  )| (   ) || (    \/   ) (      ) (   ( \   / )
+| |   | || |   ) || (___) || |         | |      | |    \ (_) / 
+| |   | || |   | ||  ___  || |         | |      | |     \   /  
+| |   | || |   ) || (   ) || |         | |      | |      ) (   
+| (___) || (__/  )| )   ( || (____/\___) (___   | |      | |   
+(_______)(______/ |/     \|(_______/\_______/   )_(      \_/                                                               
+
+ _______  _______  _______  _       _________          _______  _       
+(  ____ \(  ___  )(  ____ )( (    /|\__   __/|\     /|(  ___  )( \      
+| (    \/| (   ) || (    )||  \  ( |   ) (   | )   ( || (   ) || (      
+| |      | (___) || (____)||   \ | |   | |   | |   | || (___) || |      
+| |      |  ___  ||     __)| (\ \) |   | |   ( (   ) )|  ___  || |      
+| |      | (   ) || (\ (   | | \   |   | |    \ \_/ / | (   ) || |      
+| (____/\| )   ( || ) \ \__| )  \  |___) (___  \   /  | )   ( || (____/\
+(_______/|/     \||/   \__/|/    )_)\_______/   \_/   |/     \|(_______/
+
+
+This project is part of Udacity's VR Developer Nanodegree
+https://www.udacity.com/course/vr-developer-nanodegree--nd017
+Versions
+- Patch 2017.1.0p4
+- GVR Unity SDK v1.70.0
+
+
+EXTRA FUN STUFF:
+- Added a splash screen
+- Added a 60 sec game timer. If the player doesn't score 2000 before the end, they'll hear a sad trombone and lots of boos. Also when the timer is up, no more points can be added to the scoreboard.
+- Added a particle effect and more happy cheers when the player reaches the score of 2000.
