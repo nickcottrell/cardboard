3c5762db802af2641d6406314ea5645e0e8d9df0
nickcottrell <nick.a.cottrell@gmail.com>
2017-11-23 22:59:26 -0600

    modified project names

---
+++ Sushi Bar/Assets/Scripts/HeadRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get rotation value from phone
+		Quaternion att = Input.gyro.attitude;
+		//offset the value to account for head position
+		att = Quaternion.Euler (90f, 0f, 0f) * new Quaternion (att.x, att.y, -att.z, -att.w);
+		//assign rotation to camera
+		transform.rotation = att;
+	}
+}
---
+++ Sushi Bar/README.txt
@@ -0,0 +1,10 @@
+Nick Cottrell's Extra Project
+                    __    _    __              
+   _______  _______/ /_  (_)  / /_  ____ ______
+  / ___/ / / / ___/ __ \/ /  / __ \/ __ `/ ___/
+ (__  ) /_/ (__  ) / / / /  / /_/ / /_/ / /    
+/____/\__,_/____/_/ /_/_/  /_.___/\__,_/_/     
+
+This project is an extra project made by Nick Cottrell
+Versions
+- Patch 2017.1.0p4
