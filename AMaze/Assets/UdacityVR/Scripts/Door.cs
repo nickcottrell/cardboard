9a29057b9264091b4d7b95d11f85d3d0788079c3
nickcottrell <nick.a.cottrell@gmail.com>
2017-11-24 22:12:13 -0600

    got the key to collect and set the door variable

--- AMaze/Assets/UdacityVR/Scripts/Door.cs
+++ AMaze/Assets/UdacityVR/Scripts/Door.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Door : MonoBehaviour 
 {
     // Create a boolean value called "locked" that can be checked in OnDoorClicked() 
-	public bool locked;
+	public bool locked = true; //WHAT IS THE POINT OF THIS?
 	// Create a boolean value called "opening" that can be checked in Update() 
+	public bool keyCollected = false;
+
 
     void Update() {
         // If the door is opening and it is not fully raised
-            // Animate the door raising up
+            // Animate the door raising up	
 	}
 
     public void OnDoorClicked() {
+		if (keyCollected) {
+			Debug.Log("Opening the door...");
+				} else {
+					Debug.Log("Door is locked!");
+						
+				}
         // If the door is clicked and unlocked
             // Set the "opening" boolean to true
         // (optionally) Else
@@ -22,6 +30,9 @@ public class Door : MonoBehaviour
 
     public void Unlock()
     {
+
+		keyCollected = true;
+		locked = false;
         // You'll need to set "locked" to false here
 
     }
