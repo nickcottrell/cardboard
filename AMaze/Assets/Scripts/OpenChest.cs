ee008251edec76b4e1109c2f018a63f8676fabaf
nickcottrell <nick.a.cottrell@gmail.com>
2017-11-25 13:59:38 -0600

    finished adding functionality. matches the requirements of the rubrik

---
+++ AMaze/Assets/Scripts/OpenChest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour {
+
+
+	public Animator chestOpening;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void OnChestClick() {
+		chestOpening.enabled = true;
+	}
+}
--- AMaze/Assets/UdacityVR/Scripts/SignPost.cs
+++ AMaze/Assets/UdacityVR/Scripts/SignPost.cs
@@ -7,7 +7,6 @@ public class SignPost : MonoBehaviour
 	public void ResetScene() 
 	{
 		SceneManager.LoadScene("A Maze");
-		//Application.LoadLevel(Application.loadedLevel);
 
 	}
 }
