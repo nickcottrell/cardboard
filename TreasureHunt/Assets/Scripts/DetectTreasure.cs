0116022c7bb7e0877f634a2caa7738f7b3cc26ae
nickcottrell <nick.a.cottrell@gmail.com>
2017-12-13 21:33:48 -0800

    added a lock and key mechanic to make the game more interesting

--- TreasureHunt/Assets/Scripts/DetectTreasure.cs
+++ TreasureHunt/Assets/Scripts/DetectTreasure.cs
@@ -9,13 +9,25 @@ public class DetectTreasure : MonoBehaviour {
 	public GameObject openSound;
 	public GameObject conffetiFx;
 	public GameObject yaySounds;
+	public GameObject booSounds;
+	public GameObject lockedMsg;
+	public GameObject keyPickUpMsg;
+	public GameObject keyPrefab;
+	public GameObject youWinMsg;
 
+
+	private bool isUnlocked = false;
 
 
 	void Start() {
 		openSound.SetActive(false);
 		conffetiFx.SetActive(false);
 		yaySounds.SetActive(false);
+		booSounds.SetActive(false);
+		lockedMsg.SetActive(false);
+		keyPickUpMsg.SetActive(false);
+		youWinMsg.SetActive(false);
+
 	}
 
 	void Update()
@@ -23,11 +35,30 @@ public class DetectTreasure : MonoBehaviour {
 
 	}
 
+
+	public void unlock() {
+		isUnlocked = true;
+		Destroy (keyPrefab);
+		keyPickUpMsg.SetActive (true);
+		lockedMsg.SetActive (false);
+
+
+
+	}
+
 	public void FindTreasure() {
-		chestAnimation.enabled = true;
-		openSound.SetActive (true);
-		conffetiFx.SetActive(true);
-		yaySounds.SetActive(true);	
+		if (isUnlocked == true) {
+			chestAnimation.enabled = true;
+			openSound.SetActive (true);
+			conffetiFx.SetActive (true);
+			yaySounds.SetActive (true);	
+			youWinMsg.SetActive(true);
+
+		} else {
+			booSounds.SetActive (true);
+			lockedMsg.SetActive (true);
+		}
+
 	}
 
 }
